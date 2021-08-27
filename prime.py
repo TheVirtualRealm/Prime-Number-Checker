dbc66fd38582fd8f8e9d52adf66b75473bbce533
TheVirtualRealm <70163600+TheVirtualRealm@users.noreply.github.com>
2021-08-27 10:19:10 -0400

    Fixed the repeating numbers

--- prime.py
+++ prime.py
@@ -2,16 +2,26 @@ def PrimeNumChecker(a):
 
     if a > 0:
 
-        for j in range(2, int(a/2) + 1):
-            if (a % j) == 0:
-                print(a, "is not considered a prime number")
-                break
+        # I looked up a solution as to why my code repeats itself and was unable to figure it out
+        # I was able to figure out why the code was telling me that The number was both prime and not prime on my own
+        # I Looked up how to use code to calculate whether or not a number is prime, i also needed to look up what a prime number was lol
+        
+       
+        def rounds(b):
+            
+            while rounds == 0:
+                
+                for j in range(2, int(a/2) + 1):
+                    if (a % j) == 0:
+                        print(a, "is not considered a prime number")
+                # break
+                    else:
+                        print(a, "Is a prime number")
+                
 
-            else:
-                print(a, "Is a prime number")
-
-    
+                PrimeNumChecker(a)
+            
 
 a = int(input("Enter a number you would like me to check: "))
-
-PrimeNumChecker(a)
+    
+b + 1
